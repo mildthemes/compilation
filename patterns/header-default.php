2485b7b5d25c5fe9a914875e8c2a41dedc4c95dd
Naeem Noor <naeem@mildthemes.co>
2024-09-18 12:01:58 +0300

    version 1.0.8

--- patterns/header-default.php
+++ patterns/header-default.php
@@ -6,7 +6,7 @@
  * Block Types: core/template-part/header
  */
 ?>
-<!-- wp:group {"style":{"elements":{"link":{"color":{"text":"var:preset|color|base"}}},"dimensions":{"minHeight":"0px"},"spacing":{"padding":{"top":"0","bottom":"0","left":"var:preset|spacing|small","right":"var:preset|spacing|small"},"margin":{"top":"0","bottom":"0"},"blockGap":"0"}},"backgroundColor":"primary","textColor":"base","layout":{"type":"constrained"}} -->
+<!-- wp:group {"metadata":{"name":"Header Default"},"style":{"elements":{"link":{"color":{"text":"var:preset|color|base"}}},"dimensions":{"minHeight":"0px"},"spacing":{"padding":{"top":"0","bottom":"0","left":"var:preset|spacing|small","right":"var:preset|spacing|small"},"margin":{"top":"0","bottom":"0"},"blockGap":"0"}},"backgroundColor":"primary","textColor":"base","layout":{"type":"constrained"}} -->
 <div class="wp-block-group has-base-color has-primary-background-color has-text-color has-background has-link-color" style="min-height:0px;margin-top:0;margin-bottom:0;padding-top:0;padding-right:var(--wp--preset--spacing--small);padding-bottom:0;padding-left:var(--wp--preset--spacing--small)"><!-- wp:group {"style":{"spacing":{"padding":{"bottom":"20px","top":"20px"}}},"layout":{"type":"flex","justifyContent":"space-between"}} -->
 <div class="wp-block-group" style="padding-top:20px;padding-bottom:20px"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|x-small"}},"layout":{"type":"flex"}} -->
 <div class="wp-block-group"><!-- wp:site-logo {"width":64,"className":"is-style-default","style":{"color":[]}} /-->
@@ -16,6 +16,6 @@
 <!-- /wp:group --></div>
 <!-- /wp:group -->
 
-<!-- wp:navigation {"ref":2542,"textColor":"neutral","style":{"spacing":{"margin":{"top":"0"}},"layout":{"selfStretch":"fit","flexSize":null}},"fontSize":"x-small","layout":{"type":"flex","setCascadingProperties":true,"justifyContent":"right","orientation":"horizontal"}} /--></div>
+<!-- wp:navigation {"ref":1913,"textColor":"base","overlayBackgroundColor":"primary","style":{"spacing":{"margin":{"top":"0"}},"layout":{"selfStretch":"fit","flexSize":null}},"fontSize":"x-small","layout":{"type":"flex","setCascadingProperties":true,"justifyContent":"right","orientation":"horizontal"}} /--></div>
 <!-- /wp:group --></div>
 <!-- /wp:group -->
